eab587e67eab56114457e5c1c98557de9d5b5f04
Desleyo <desleyoord@gmail.com>
2025-03-23 20:37:26 +0100

    Clients now have synced match scores

--- Websocket/Server/index.js
+++ Websocket/Server/index.js
@@ -1,12 +1,17 @@
 const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 8082 });
+let cache = null;
 
 wss.on("connection", ws => {
     console.log("New client connected");
+    if (cache != null) {
+        wss.broadcast(cache);
+    }
 
     ws.on("message", data => {
         console.log(`Data send: ${data}`);
+        cache = data;
 
         wss.broadcast(data);
     });
