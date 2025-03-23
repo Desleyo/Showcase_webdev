26d8a5fa1fb814205708f8e255d06dc87f078102
Desleyo <desleyoord@gmail.com>
2025-03-23 18:26:49 +0100

    Cypress register and login tests

---
+++ Showcase/Testing/cypress/e2e/Login.cy.js
@@ -0,0 +1,45 @@
+﻿const url = 'https://localhost:7110/';
+
+context('Login', () => {
+    beforeEach(() => {
+        //Visit home page
+        cy.visit(url)
+
+        //Find login button and click, also check the page
+        cy.contains('Login').click()
+        cy.url().should('include', '/Account/Login')
+    })
+
+    it('Login without 2FA', () => {
+        //Fill in input fields and submit
+        cy.get('form').within(() => {
+            cy.get('input[type=email]').type('admin@test.com')
+            cy.get('input[type=password]').type('Test-123')
+            cy.get('button[type=submit]').click()
+        });
+
+        //Check if back on homepage and logged in
+        cy.url().should('eq', url)
+        cy.contains('Hello admin@test.com!')
+        cy.contains('Logout')
+    })
+
+    it('Reach 2FA page when logging in', () => {
+        //Fill in input fields and submit
+        cy.get('form').within(() => {
+            cy.get('input[type=email]').type('desleyoord@gmail.com')
+            cy.get('input[type=password]').type('Test-123')
+            cy.get('button[type=submit]').click()
+        });
+
+        //Check if on 2FA page
+        cy.url().should('include', '/LoginWith2fa')
+
+        //Check if form correct
+        cy.get('form').within(() => {
+            cy.get('input[type=text]')
+            cy.get('input[type=checkbox]')
+            cy.get('button[type=submit]')
+        })
+    })
+})
---
+++ Showcase/Testing/cypress/e2e/Register.cy.js
@@ -0,0 +1,52 @@
+﻿const url = 'https://localhost:7110/';
+
+context('Register', () => {
+    beforeEach(() => {
+        //Visit home page
+        cy.visit(url)
+
+        //Find register button and click, also check the page
+        cy.contains('Register').click()
+        cy.url().should('include', '/Account/Register')
+    })
+
+    it('Register invalid email', () => {
+        //Wrongly fill in email and submit
+        cy.get('form').within(() => {
+            cy.get('input[type=email]').type('test@')
+            cy.get('button[type=submit]').click()
+        })
+
+        //Check for error
+        cy.get('form').within(() => {
+            cy.get('span[id=Input_Email-error]')
+        })
+    })
+
+    it('Register invalid password', () => {
+        //Wrongly fill in password, and submit
+        cy.get('form').within(() => {
+            cy.get('input[id=Input_Password]').type('pass')
+            cy.get('button[type=submit]').click()
+        })
+
+        //Check for error
+        cy.get('form').within(() => {
+            cy.get('span[id=Input_Password-error]')
+        })
+    })
+
+    it('Register non-matching passwords', () => {
+        //Fill password fields with non-matching passwords
+        cy.get('form').within(() => {
+            cy.get('input[id=Input_Password]').type('pass123')
+            cy.get('input[id=Input_ConfirmPassword]').type('123pass')
+            cy.get('button[type=submit]').click()
+        })
+
+        //Check for error
+        cy.get('form').within(() => {
+            cy.get('span[id=Input_ConfirmPassword-error]')
+        })
+    })
+})
